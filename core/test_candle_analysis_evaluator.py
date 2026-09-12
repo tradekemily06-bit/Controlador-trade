05d99e995cb0d872359206a1c7c3d8c1d55cc591
tradekemily06-bit <tradekemily06@gmail.com>
2026-09-12 14:07:31 -0300

    test: clean candle evaluator imports

--- core/test_candle_analysis_evaluator.py
+++ core/test_candle_analysis_evaluator.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timezone
 
 from core.candle_analysis_evaluator import evaluate_candle_snapshot
 from core.models import Signal
