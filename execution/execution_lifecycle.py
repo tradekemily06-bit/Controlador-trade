564f62d07f544acfac2d19ee2edf55dce77e8a30
tradekemily06-bit <tradekemily06@gmail.com>
2026-09-21 19:11:12 -0300

    Harden P6 crash durability: fsync lifecycle parent directory after atomic replace

--- execution/execution_lifecycle.py
+++ execution/execution_lifecycle.py
@@ -143,6 +143,15 @@ class ExecutionLifecycleStore:
             finally:
                 self._release_lock(lock_file)
 
+    def _fsync_directory(self) -> None:
+        if os.name != "posix":
+            return
+        directory_fd = os.open(self.path.parent, os.O_RDONLY)
+        try:
+            os.fsync(directory_fd)
+        finally:
+            os.close(directory_fd)
+
     @staticmethod
     def _acquire_lock(lock_file) -> None:
         if fcntl is not None:
@@ -191,3 +200,4 @@ class ExecutionLifecycleStore:
             temp_file.flush()
             os.fsync(temp_file.fileno())
         os.replace(temporary, self.path)
+        self._fsync_directory()
