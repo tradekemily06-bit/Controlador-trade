b8547e20e3c6958ef7711e19b713764502fa103b
tradekemily06-bit <tradekemily06@gmail.com>
2026-09-20 23:45:09 -0300

    security: flush final ledger after atomic replace

--- execution/execution_ledger.py
+++ execution/execution_ledger.py
@@ -81,6 +81,8 @@ class ExecutionLedger:
         with temporary.open("rb") as handle:
             os.fsync(handle.fileno())
         os.replace(temporary, self.path)
+        with self.path.open("rb") as handle:
+            os.fsync(handle.fileno())
         if os.name != "nt":
             directory_fd = os.open(self.path.parent, os.O_RDONLY)
             try:
