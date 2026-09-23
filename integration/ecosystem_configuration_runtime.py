1974259210fa89dbb78996a677331c0a274790e1
tradekemily06-bit <tradekemily06@gmail.com>
2026-09-23 02:12:11 -0300

    feat: expose persistent ecosystem use mode

--- integration/ecosystem_configuration_runtime.py
+++ integration/ecosystem_configuration_runtime.py
@@ -64,6 +64,7 @@ class ConfiguredEcosystemService(EcosystemService):
         value = self.preferences.preferences
         result = asdict(value)
         result["chart_theme"] = value.chart_theme.value
+        result["use_mode"] = value.use_mode.value
         result["candle"]["style"] = value.candle.style.value
         result["candle"]["color_mode"] = value.candle.color_mode.value
         return result
